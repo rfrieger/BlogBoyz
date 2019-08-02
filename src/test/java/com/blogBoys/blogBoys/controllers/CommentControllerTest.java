cb9e9dce86b7c692d7710f518eeee0ff8bd202ec
rfrieger <rfrieger@gmail.com>
2019-08-02 11:12:56 -0400

    added author to comment

---
+++ src/test/java/com/blogBoys/blogBoys/controllers/CommentControllerTest.java
@@ -0,0 +1,13 @@
+package com.blogBoys.blogBoys.controllers;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CommentControllerTest {
+
+    @Test
+    public void creatComment() {
+    }
+
+}
---
+++ src/test/java/com/blogBoys/blogBoys/models/UsersRequestTest.java
@@ -0,0 +1,32 @@
+package com.blogBoys.blogBoys.models;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class UsersRequestTest {
+
+    @Test
+    public void getUser_id() {
+    }
+
+    @Test
+    public void setUser_id() {
+    }
+
+    @Test
+    public void getName() {
+    }
+
+    @Test
+    public void setName() {
+    }
+
+    @Test
+    public void getPassword() {
+    }
+
+    @Test
+    public void setPassword() {
+    }
+}
